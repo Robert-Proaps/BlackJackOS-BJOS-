4255c663df04c21bb135605a3e158d444487442f
Robert Proaps <sethproaps@gmail.com>
2026-03-09 19:25:21 -0400

    Corrected Splashscreen from RGB565 to BGR565

--- BlackJackOS/BlackJackOS.ino
+++ BlackJackOS/BlackJackOS.ino
@@ -3,50 +3,46 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>
 #include "utilities.h"
-#include "BJOS_Splashscreen.h"
+#include "BJOS_SplashscreenBGR565.h"
 
 //ST7789 LCD Controller SPI Command Table
 typedef struct {
-    uint8_t cmd;
-    uint8_t data[14];
-    uint8_t len;
+  uint8_t cmd;
+  uint8_t data[14];
+  uint8_t len;
 } lcd_cmd_t;
 
 lcd_cmd_t lcd_st7789v[] = {
-    {0x01, {0}, 0 | 0x80},
-    {0x11, {0}, 0 | 0x80},
-    {0x3A, {0X05}, 1},
-    {0x36, {0x55}, 1},
-    {0xB2, {0x0C, 0x0C, 0X00, 0X33, 0X33}, 5},
-    {0xB7, {0X75}, 1},
-    {0xBB, {0X1A}, 1},
-    {0xC0, {0X2C}, 1},
-    {0xC2, {0X01}, 1},
-    {0xC3, {0X13}, 1},
-    {0xC4, {0X20}, 1},
-    {0xC6, {0X0F}, 1},
-    {0xD0, {0XA4, 0XA1}, 2},
-    {0xD6, {0XA1}, 1},
-    {0xE0, {0XD0, 0X0D, 0X14, 0X0D, 0X0D, 0X09, 0X38, 0X44, 0X4E, 0X3A, 0X17, 0X18, 0X2F, 0X30}, 14},
-    {0xE1, {0XD0, 0X09, 0X0F, 0X08, 0X07, 0X14, 0X37, 0X44, 0X4D, 0X38, 0X15, 0X16, 0X2C, 0X3E}, 14},
-    {0x21, {0}, 0}, //invertDisplay
-    {0x29, {0}, 0},
-    {0x2C, {0}, 0},
+  { 0x01, { 0 }, 0 | 0x80 },
+  { 0x11, { 0 }, 0 | 0x80 },
+  { 0x3A, { 0X05 }, 1 },
+  { 0x36, { 0x55 }, 1 },
+  { 0xB2, { 0x0C, 0x0C, 0X00, 0X33, 0X33 }, 5 },
+  { 0xB7, { 0X75 }, 1 },
+  { 0xBB, { 0X1A }, 1 },
+  { 0xC0, { 0X2C }, 1 },
+  { 0xC2, { 0X01 }, 1 },
+  { 0xC3, { 0X13 }, 1 },
+  { 0xC4, { 0X20 }, 1 },
+  { 0xC6, { 0X0F }, 1 },
+  { 0xD0, { 0XA4, 0XA1 }, 2 },
+  { 0xD6, { 0XA1 }, 1 },
+  { 0xE0, { 0XD0, 0X0D, 0X14, 0X0D, 0X0D, 0X09, 0X38, 0X44, 0X4E, 0X3A, 0X17, 0X18, 0X2F, 0X30 }, 14 },
+  { 0xE1, { 0XD0, 0X09, 0X0F, 0X08, 0X07, 0X14, 0X37, 0X44, 0X4D, 0X38, 0X15, 0X16, 0X2C, 0X3E }, 14 },
+  { 0x21, { 0 }, 0 },  //invertDisplay
+  { 0x29, { 0 }, 0 },
+  { 0x2C, { 0 }, 0 },
 };
 
-TFT_eSPI tft; //TFT LCD Object
+TFT_eSPI tft;  //TFT LCD Object
 
 void setup() {
   Serial.begin(115200);
   systemStartup();
-
-
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
 
 //Functions
@@ -79,7 +75,7 @@ void systemStartup() {
   Serial.println("Screen Initializing.");
   tft.begin();
 
-  #if 0
+#if 0
     for (uint8_t i = 0; i < (sizeof(lcd_st7789v) / sizeof(lcd_cmd_t)); i++) {
         tft.writecommand(lcd_st7789v[i].cmd);
         for (int j = 0; j < (lcd_st7789v[i].len & 0x7f); j++) {
@@ -90,44 +86,43 @@ void systemStartup() {
             delay(120);
         }
     }
-  #endif
+#endif
 
   tft.setRotation(1);
   pinMode(BOARD_BL_PIN, OUTPUT);
   setBrightness(8);
-  delay(30);//Might be critical delay
+
 
   Serial.println("Startup Complete!");
   //Show Splashscreen
   tft.fillScreen(TFT_BLUE);
-  delay(100);
-  tft.pushImage(0, 0, SPLASH_WIDTH, SPLASH_HEIGHT, (const uint16_t*)BJOS_Splashscreen_img);
+  delay(1000);
+  tft.pushImage(0, 0, SPLASH_WIDTH, SPLASH_HEIGHT, (const uint16_t*)BJOS_SplashscreenBGR_img);
 }
 
 // LCD Backlight Control
 // LilyGo  T-Deck  control backlight chip has 16 levels of adjustment range
 // The adjustable range is 0~15, 0 is the minimum brightness, 16 is the maximum brightness
-void setBrightness(uint8_t value)
-{
-    static uint8_t level = 0;
-    static uint8_t steps = 16;
-    if (value == 0) {
-        digitalWrite(BOARD_BL_PIN, 0);
-        delay(3);
-        level = 0;
-        return;
-    }
-    if (level == 0) {
-        digitalWrite(BOARD_BL_PIN, 1);
-        level = steps;
-        delayMicroseconds(30);
-    }
-    int from = steps - level;
-    int to = steps - value;
-    int num = (steps + to - from) % steps;
-    for (int i = 0; i < num; i++) {
-        digitalWrite(BOARD_BL_PIN, 0);
-        digitalWrite(BOARD_BL_PIN, 1);
-    }
-    level = value;
+void setBrightness(uint8_t value) {
+  static uint8_t level = 0;
+  static uint8_t steps = 16;
+  if (value == 0) {
+    digitalWrite(BOARD_BL_PIN, 0);
+    delay(3);
+    level = 0;
+    return;
+  }
+  if (level == 0) {
+    digitalWrite(BOARD_BL_PIN, 1);
+    level = steps;
+    delayMicroseconds(30);
+  }
+  int from = steps - level;
+  int to = steps - value;
+  int num = (steps + to - from) % steps;
+  for (int i = 0; i < num; i++) {
+    digitalWrite(BOARD_BL_PIN, 0);
+    digitalWrite(BOARD_BL_PIN, 1);
+  }
+  level = value;
 }
